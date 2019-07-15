9dcb6ac9107d84a637661398afbf0803967f4e02
Oduor Samuel <soduor15@yahoo.com>
2019-07-15 12:16:16 +0300

    created user model

---
+++ models/user.js
@@ -0,0 +1,60 @@
+const mongoose=require("mongoose");
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+const Schema= mongoose.Schema;
+
+let userSchema=new Schema({
+first_name:{
+     type:String,
+     required:true
+ },
+ last_name:{
+     type:String,
+     required:true   
+ },
+ user_name:{
+    type:String,
+    default:null
+},
+email:{
+    type:String, 
+    required:true,
+},
+ address:{
+    type:String,
+    required:true 
+},
+mobile_number:{
+    type:Number,
+    required:true 
+},
+telephone_number:{
+    type:Number,
+    required:true 
+},
+ gender:{
+     type:String,
+     required:true
+ },
+ IsDeleted: {
+    type: Number,
+    default: 0
+},
+ DeletedAt:{
+    type:Date,  
+    default:null
+},
+DeletedBy:{
+    type:String,  
+    default:null
+},
+updated_at:{
+    type:Date,  
+    default:null
+},
+ updated_by:{
+     type:String,
+     default:null
+ }
+},
+{ versionKey: false })
+module.exports=Users=mongoose.model('Users',userSchema);
